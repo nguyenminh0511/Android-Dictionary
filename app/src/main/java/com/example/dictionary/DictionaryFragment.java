d398428dca0af776465606371c80909e2e0d4dea
nguyenminh0511 <65353665+nguyenminh0511@users.noreply.github.com>
2021-12-07 18:50:09 +0700

    Add List View for Dictionary Fragment

--- app/src/main/java/com/example/dictionary/DictionaryFragment.java
+++ app/src/main/java/com/example/dictionary/DictionaryFragment.java
@@ -10,7 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class DictionaryFragment extends Fragment {
 
@@ -46,15 +49,57 @@ public class DictionaryFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button myButton = (Button) view.findViewById(R.id.myBtn);
-        myButton.setOnClickListener(new View.OnClickListener() {
+//        Button myButton = (Button) view.findViewById(R.id.myBtn);
+//        myButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (listener != null) {
+//                    listener.onItemClick(value);
+//                }
+//            }
+//        });
+
+        ListView dictList = view.findViewById(R.id.dictionaryList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_list_item_1, getListOfWords());
+        dictList.setAdapter(adapter);
+        dictList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (listener != null) {
-                    listener.onItemClick(value);
+                    listener.onItemClick(getListOfWords()[position]);
                 }
             }
         });
+    }
+
+    String[] getListOfWords() {
+        String[] source = new String[] {
+                "a",
+                "abadon",
+                "ability",
+                "able",
+                "about",
+                "above",
+                "aboard",
+                "absense",
+                "absent",
+                "absolute",
+                "absolutely",
+                "absorb",
+                "abuse",
+                "academic",
+                "accent",
+                "accept",
+                "acceptable",
+                "access",
+                "accident",
+                "accidental",
+                "accidentally",
+                "accommodation",
+                "accompany"
+        };
+        return source;
     }
 
     public void setOnFragmentListener(FragmentListener listener) {
